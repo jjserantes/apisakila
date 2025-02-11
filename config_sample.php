def2fb155c676aced19b7b01c19ba8653140bd6f
jjserantes <jserantes@gmail.com>
2025-02-11 20:05:37 +0100

    primera

---
+++ config_sample.php
@@ -0,0 +1,6 @@
+<?php
+define(constant_name:'HOST', value:"localhost");
+define(constant_name:'DB_USER', value:"root");
+define(constant_name:'DB_PASS', value:"");
+define(constant_name:'DB_DATABASE', value:"sakila");
+?>
